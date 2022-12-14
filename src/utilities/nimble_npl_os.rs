981a431eed8ca18817925bb26aa4197338b25875
taks <857tn859@gmail.com>
2022-12-14 19:29:56 +0900

    fix compile error for riscv32imc-esp-espidf target

--- src/utilities/nimble_npl_os.rs
+++ src/utilities/nimble_npl_os.rs
@@ -5,11 +5,27 @@ use esp_idf_sys::{
 #[inline]
 #[allow(unused)]
 pub(crate) fn ble_npl_hw_enter_critical() {
-  unsafe { xPortEnterCriticalTimeout(&mut ble_port_mutex, portMUX_NO_TIMEOUT) };
+  #[cfg(esp32c3)]
+  unsafe {
+    vPortEnterCritical();
+  }
+
+  #[cfg(not(esp32c3))]
+  unsafe {
+    xPortEnterCriticalTimeout(&mut ble_port_mutex, portMUX_NO_TIMEOUT)
+  };
 }
 
 #[inline]
 #[allow(unused)]
 pub(crate) fn ble_npl_hw_exit_critical() {
-  unsafe { vPortExitCritical(&mut ble_port_mutex) };
+  #[cfg(esp32c3)]
+  unsafe {
+    vPortExitCritical();
+  }
+
+  #[cfg(not(esp32c3))]
+  unsafe {
+    vPortExitCritical(&mut ble_port_mutex)
+  };
 }

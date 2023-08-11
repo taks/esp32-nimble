c4db3c5a7ab04084ef18832ef180082c224d8c26
taks <857tn859@gmail.com>
2023-08-11 12:24:57 +0900

    Added documentation

--- src/server/on_write_args.rs
+++ src/server/on_write_args.rs
@@ -6,10 +6,14 @@ pub struct OnWriteArgs<'a> {
 }
 
 impl OnWriteArgs<'_> {
+  /// If the reject is called, no value is written to BLECharacteristic or BLEDescriptor.
+  /// A write error (0xFF) is sent to the sender.
   pub fn reject(&mut self) {
     self.reject_with_error_code(0xFF);
   }
 
+  /// If the reject is called, no value is written to BLECharacteristic or BLEDescriptor.
+  /// The argument error code is sent to the sender.
   pub fn reject_with_error_code(&mut self, error_code: u8) {
     self.reject = true;
     self.error_code = error_code;

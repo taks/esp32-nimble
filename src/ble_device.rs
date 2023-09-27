f7859ddac889bf5784bab13cae74e3278940998c
taks <857tn859@gmail.com>
2023-09-27 21:00:43 +0900

    Fix fmt

--- src/ble_device.rs
+++ src/ble_device.rs
@@ -18,9 +18,7 @@ static mut BLE_DEVICE: Lazy<BLEDevice> = Lazy::new(|| {
   }
 });
 static mut BLE_SCAN: Lazy<BLEScan> = Lazy::new(BLEScan::new);
-pub static mut BLE_SERVER: Lazy<BLEServer> = Lazy::new(|| {
-  BLEServer::new()
-});
+pub static mut BLE_SERVER: Lazy<BLEServer> = Lazy::new(|| BLEServer::new());
 static mut BLE_ADVERTISING: Lazy<BLEAdvertising> = Lazy::new(BLEAdvertising::new);
 
 pub static mut OWN_ADDR_TYPE: u8 = esp_idf_sys::BLE_OWN_ADDR_PUBLIC as _;

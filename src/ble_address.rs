725247b24c1da337d001984b5b7f2774c26ca665
taks <857tn859@gmail.com>
2024-09-10 08:24:08 +0900

    clippy

--- src/ble_address.rs
+++ src/ble_address.rs
@@ -36,7 +36,7 @@ impl BLEAddress {
     let mut val = [0u8; 6];
 
     let mut nth = 0;
-    for byte in input.split(|c| c == ':' || c == '-') {
+    for byte in input.split([':', '-']) {
       if nth == 6 {
         return None;
       }
